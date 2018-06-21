864f667175df598cedae09d24b6d0379dc8e91f5
mikeenders77 <anthonycmark@gmail.com>
2018-06-21 04:31:03 -0400

    first commit

---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'
+require 'mina/whenever'
+require 'mina/systemd'
+
+set :domain, 'rightitjobs.com'
+set :deploy_to, '/home/deploy/csvsail'
+set :repository, 'git@github.com:mikeenders77/csvsail.git'
+set :branch, 'master'
+set :rails_env, 'production'
+set :user, 'deploy'
+
+set :shared_dirs, fetch(:shared_dirs, []).push('tmp/pids', 'tmp/sockets')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/puma.rb', 'config/secrets.yml')
+
+task :environment do
+  invoke :'rbenv:load'
+end
+
+task setup: :environment do
+  command %[mkdir -p "#{fetch(:shared_path)}/config"]
+  command %[chmod g+rx,u+rwx "#{fetch(:shared_path)}/config"]
+
+  comment %{Be sure to add 'database.yml', 'secrets.yml' and 'puma.rb' in '#{fetch(:shared_path)}/config/' directory}
+end
+
+desc "Deploys the current version to the server."
+task deploy: :environment do
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+    on :launch do
+      invoke :'systemctl:restart', 'csvsail-puma'
+      invoke :'systemctl:restart', 'csvsail-bg-worker'
+      invoke :'whenever:update'
+    end
+  end
+end
