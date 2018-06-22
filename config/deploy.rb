8efea1c6e12701d89aeba7851334042b4fd7f146
mikeenders77 <anthonycmark@gmail.com>
2018-06-22 05:23:42 -0400

    change back to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.5.0"
+lock "~> 3.7.0"
 
 set :application, "csvsail"
 set :repo_url, "git@github.com:mikeenders77/csvsail.git"
